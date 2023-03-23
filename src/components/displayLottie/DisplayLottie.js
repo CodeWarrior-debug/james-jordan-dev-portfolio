4355687ea27b8dde87a341e0918b3116ad28718f
James Jordan <jpjordan1987@gmail.com>
2023-03-23 14:04:19 -0400

    removed Lottie dependency

--- src/components/displayLottie/DisplayLottie.js
+++ src/components/displayLottie/DisplayLottie.js
@@ -1,20 +1,20 @@
-import React, {Component, Suspense} from "react";
-import Lottie from "react-lottie";
-import Loading from "../../containers/loading/Loading";
+// import React, {Component, Suspense} from "react";
+// import Lottie from "react-lottie";
+// import Loading from "../../containers/loading/Loading";
 
-export default class DisplayLottie extends Component {
-  render() {
-    const animationData = this.props.animationData;
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData
-    };
+// export default class DisplayLottie extends Component {
+//   render() {
+//     const animationData = this.props.animationData;
+//     const defaultOptions = {
+//       loop: true,
+//       autoplay: true,
+//       animationData: animationData
+//     };
 
-    return (
-      <Suspense fallback={<Loading />}>
-        <Lottie options={defaultOptions} isClickToPauseDisabled={true} />
-      </Suspense>
-    );
-  }
-}
+//     return (
+//       <Suspense fallback={<Loading />}>
+//         <Lottie options={defaultOptions} isClickToPauseDisabled={true} />
+//       </Suspense>
+//     );
+//   }
+// }
